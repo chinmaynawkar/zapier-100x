338ff7d188b5a2e90dfcd5e56b4a8cfc02e7466e
chinmaynawkar <cnawkar19@gmail.com>
2024-07-13 20:24:31 +0530

    chore:some fixes

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -3,10 +3,19 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = process.env.BASE_URL;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
-});
+// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
+// password logic
+app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
 
+    // store in db a new trigger
+
+    // push it on to a queue (kafka/redis)
+
+    
+
+})
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
